8c5f15c9a64206004418b7321246555f99a9b97c
Indula Perera <105506303+Indula-Perera@users.noreply.github.com>
2025-03-26 11:12:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Streamlit UI
-st.title("Unique data Finder")
+st.title("Unique Data Finder")
 
 # Upload Correct Data File
 st.write("### Upload Correct Data File")
